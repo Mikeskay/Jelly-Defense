fd720915b4fc6c40b84fd74357b096160b0d4791
Mikeskay <47972946+Mikeskay@users.noreply.github.com>
2022-03-21 22:09:19 +0000

    Create JellyTest.java

---
+++ test/JellyTest.java
@@ -0,0 +1,19 @@
+package jellydefense.test;
+
+import jellydefense.jellies.*;
+import jellydefense.towers.*;
+
+
+public class JellyTest {
+    public static void main(String[] args) {
+        
+        RedJelly redJelly = new RedJelly(101);
+        BlueJelly blueJelly = new BlueJelly(-1);
+
+
+        System.out.println(blueJelly);
+        System.out.println(redJelly);
+
+    }
+    
+}
